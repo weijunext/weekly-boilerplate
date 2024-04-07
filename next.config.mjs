e5c88035aed789f0f34d34f9b2db3fe325526bc9
WEI JUN <weijunext@gmail.com>
2024-04-07 23:03:04 +0800

    chore: mdx

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+import createMDX from "@next/mdx";
+import remarkGfm from "remark-gfm";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions`` to include MDX files
+  pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
+};
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [],
+  },
+});
+
+// Wrap MDX and Next.js config with each other
+export default withMDX(nextConfig);
