3d2a41b7c64bca2667318df9f5a1e4d08c8addc5
WEI JUN <weijunext@gmail.com>
2024-04-08 18:23:23 +0800

    feat: developer card

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import DeveloperCard from "@/components/DeveloperCard";
 import WeeklyList from "@/components/WeeklyList";
 
 export default function Home() {
   return (
-    <>
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-24 text-center">
+    <div className="flex flex-col md:flex-row w-full pb-16 pt-24">
+      <div className="hidden md:block md:w-1/5"></div>
+      <div className="w-full md:w-3/5">
         <WeeklyList />
+        <DeveloperCard />
       </div>
-    </>
+      <div className="hidden md:block md:w-1/5 text-right pr-6">
+        按年/月展示的时间线
+      </div>
+    </div>
   );
 }
