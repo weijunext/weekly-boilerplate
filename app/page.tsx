6c7a9e496d139f72ebc4e51722f2892fd41c350b
WEI JUN <weijunext@gmail.com>
2024-04-09 02:14:26 +0800

    style: timeline sticky

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import WeeklyList from "@/components/WeeklyList";
 
 export default function Home() {
   return (
-    <div className="flex flex-col md:flex-row w-full pt-24">
+    <div className="flex flex-row w-full pt-12">
       <div className="hidden md:block md:w-1/5"></div>
       <div className="w-full md:w-3/5 px-6">
         <WeeklyList />
         <DeveloperCard />
       </div>
-      <div className="absolute right-0 hidden md:flex justify-end md:w-1/5 pr-6 text-right">
+      <div className=" hidden md:flex justify-end md:w-1/5 pr-6 text-right">
         <TimeLine></TimeLine>
       </div>
     </div>
--- components/TimeLine.tsx
+++ components/TimeLine.tsx
@@ -8,7 +8,10 @@ export default async function TimeLine() {
     await getWeeklyPosts();
 
   return (
-    <ScrollArea className="h-72 w-32 rounded-md border border-gray-600">
+    <ScrollArea
+      className="h-72 w-32 rounded-md border border-gray-600 sticky top-0"
+      style={{ position: "sticky" }}
+    >
       <div className="p-4">
         <h4 className="mb-4 text-sm font-medium leading-none">
           <Link href="/">时间线</Link>
