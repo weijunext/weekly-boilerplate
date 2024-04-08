e4017d11b5b0a28b6d98233d3b102fd7e9d6bf76
WEI JUN <weijunext@gmail.com>
2024-04-09 00:00:01 +0800

    feat: update footer bg

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
 
   return (
     <footer>
-      <div className="mt-16 space-y-2 pt-6 pb-4 flex flex-col items-center bg-black text-sm text-gray-400 border-t">
+      <div className="mt-16 space-y-2 pt-6 pb-4 flex flex-col items-center text-sm text-gray-400 border-t border-gray-600">
         <FooterLinks />
         <FooterProducts />
         <div className="flex space-x-2">
