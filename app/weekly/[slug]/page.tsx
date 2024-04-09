1159b61c3f735878abbadd26c500d3f0ce9b8230
WEI JUN <weijunext@gmail.com>
2024-04-09 12:01:19 +0800

    feat: 文章底部相关链接

--- app/weekly/[slug]/page.tsx
+++ app/weekly/[slug]/page.tsx
@@ -1,11 +1,14 @@
 import ArticleIndex from "@/components/ArticleIndex";
 import MDXComponents from "@/components/MDXComponents";
 import WeeklyList from "@/components/WeeklyList";
+import { Separator } from "@/components/ui/separator";
 import { WeeklyPost, getWeeklyPosts } from "@/lib/getWeeklyPosts";
 import "@/styles/mdx.css";
+import dayjs from "dayjs";
 import fs from "fs";
 import matter from "gray-matter";
 import { MDXRemote } from "next-mdx-remote/rsc";
+import Link from "next/link";
 
 type Props = {
   params: {
@@ -16,10 +19,14 @@ type Props = {
 export default async function WeeklyDetailsPage({ params }: Props) {
   const { slug } = params;
   const { posts }: { posts: WeeklyPost[] } = await getWeeklyPosts();
-  const post = posts.find((post) => post.metadata.slug === slug);
+  const postIndex = posts.findIndex((post) => post.metadata.slug === slug);
+  const post = posts[postIndex];
+  // Reverse list order, thus invert condition check
+  const nextPost = postIndex - 1 >= 0 ? posts[postIndex - 1] : null;
+  const prevPost = postIndex + 1 < posts.length ? posts[postIndex + 1] : null;
 
   if (!post) {
-    // TODO: 404页面
+    // TODO: 404
     return <div>TODO: not found</div>;
   }
 
@@ -30,10 +37,52 @@ export default async function WeeklyDetailsPage({ params }: Props) {
       <aside className="hidden md:block md:w-1/5 pl-6 max-h-[100vh] h-full overflow-auto">
         <WeeklyList isSide />
       </aside>
-      <article className="w-full md:w-3/5 px-6" id={`article`}>
-        <h1>{metadata.title}</h1>
-        <MDXRemote source={source} components={MDXComponents} />
-      </article>
+      <div className="w-full md:w-3/5 px-6">
+        <article id={`article`}>
+          <h1>{metadata.title}</h1>
+          <MDXRemote source={source} components={MDXComponents} />
+        </article>
+        <Separator className="my-12 bg-gray-600" />
+        <div className="flex justify-between">
+          <div>发布时间：{dayjs(metadata.date).format("YYYY-MM-DD")}</div>
+          <div className="flex gap-2 flex-col sm:flex-row">
+            {prevPost ? (
+              <Link
+                href={prevPost.metadata.slug}
+                className="text-[#9bdbee] hover:text-[#ffce55] underline"
+              >
+                上一篇
+              </Link>
+            ) : (
+              <></>
+            )}
+            {nextPost ? (
+              <Link
+                href={nextPost.metadata.slug}
+                className="text-[#9bdbee] hover:text-[#ffce55] underline"
+              >
+                下一篇
+              </Link>
+            ) : (
+              <></>
+            )}
+            <Link
+              href="/"
+              className="text-[#9bdbee] hover:text-[#ffce55] underline"
+            >
+              去首页
+            </Link>
+            <Link
+              href="https://twitter.com/weijunext/"
+              target="_blank"
+              rel="noopener noreferrer nofollow"
+              className="text-[#9bdbee] hover:text-[#ffce55] underline"
+            >
+              Twitter/X
+            </Link>
+          </div>
+        </div>
+      </div>
       <div className="hidden md:flex flex-col justify-start md:w-1/5 pr-6">
         <ArticleIndex />
       </div>
