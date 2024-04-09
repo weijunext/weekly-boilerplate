d3c42f750e6ed62393046aec24aba0c11ab43b17
WEI JUN <weijunext@gmail.com>
2024-04-09 09:40:26 +0800

    feat: update getWeeklyPosts

--- app/weekly/[slug]/page.tsx
+++ app/weekly/[slug]/page.tsx
@@ -10,34 +10,32 @@ type Props = {
   };
 };
 
+export default async function WeeklyDetailsPage({ params }: Props) {
+  const { slug } = params;
+  const { posts }: { posts: WeeklyPost[] } = await getWeeklyPosts();
+  const post = posts.find((post) => post.metadata.slug === slug);
+
+  if (!post) {
+    // TODO: 404页面
+    return <div>TODO: not found</div>;
+  }
+
+  const { source, metadata } = await getPostDetails(post.fullPath);
+
+  return (
+    <article className="w-full">
+      <h1>{metadata.title}</h1>
+      <MDXRemote source={source} />
+    </article>
+  );
+}
+
 export async function generateStaticParams() {
   const { posts }: { posts: WeeklyPost[] } = await getWeeklyPosts();
 
   return posts.map((post) => ({
     slug: post.metadata.slug,
   }));
-}
-
-export default async function WeeklyDetailsPage({ params }: Props) {
-  const { slug } = params;
-
-  const { posts }: { posts: WeeklyPost[] } = await getWeeklyPosts();
-
-  const post = posts.find((post) => post.metadata.slug === slug);
-
-  if (!post) {
-    // TODO: 404页面
-    return <div>Post not found</div>;
-  }
-
-  const { source, metadata } = await getPostDetails(post.fullPath);
-
-  return (
-    <article>
-      <h1>{metadata.title}</h1>
-      <MDXRemote source={source} />
-    </article>
-  );
 }
 
 async function getPostDetails(fullPath: string): Promise<{
